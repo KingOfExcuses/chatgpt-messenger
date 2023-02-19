5f39b810d120e6a1c594c5f3ca5a6276ef7ca157
BMullins <6474546+KingOfExcuses@users.noreply.github.com>
2023-02-19 17:58:53 -0600

    Added GoogeAuth  - Can now sign in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,34 @@
+import { getServerSession } from "next-auth";
+import Login from "../Components/Login";
+import ProvidersWrapper from "../Components/ProvidersWrapper";
 import Sidebar from "../Components/Sidebar";
+import { authOptions } from "../server/auth";
 import "../styles/globals.css";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html>
       <head />
       <body>
-        <div className="flex">
-          <Sidebar />
+        <ProvidersWrapper session={session}>
+          {!session ? (
+            <Login />
+          ) : (
+            <div className="flex">
+              <Sidebar />
 
-          {/* Loading Notification */}
+              {/* Loading Notification */}
 
-          <div className="flex-1 bg-dark">{children}</div>
-        </div>
+              <div className="flex-1 bg-dark">{children}</div>
+            </div>
+          )}
+        </ProvidersWrapper>
       </body>
     </html>
   );
