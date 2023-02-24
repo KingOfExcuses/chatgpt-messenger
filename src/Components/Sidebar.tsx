067fc09606fe68be8ce23c823db0b215cd97c0c6
BMullins <6474546+KingOfExcuses@users.noreply.github.com>
2023-02-23 19:23:29 -0600

    Updated Login, added sign out

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,13 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
 import NewChat from "./NewChat";
 
 function Sidebar() {
+  const { data: session } = useSession();
+
   return (
     <div className="flex h-screen max-w-xs flex-col overflow-y-auto bg-gray p-2 md:min-w-[260px]">
       <div className="flex-1">
         <NewChat />
         {/* OpenChats */}
         <div>{/* Model Selection */}</div>
+        {/* Map through chat rows */}
       </div>
+      {session && (
+        <img
+          onClick={() => void signOut()}
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          src={session.user.image!}
+          alt="User Picture"
+          className="mx-auto h-12 w-12 cursor-pointer rounded-full"
+        />
+      )}
     </div>
   );
 }
